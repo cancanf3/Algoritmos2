9cb5a76dd3ce6d753cfdf843e1b488d9ecaddcbb
adgalad <carlos.25896@gmail.com>
2013-12-13 20:52:34 -0430

    mergesort

--- main.py
+++ main.py
@@ -1,37 +1,10 @@
 #! /usr/bin/env python
-# Programa de prueba
+# Modulo de Tubo
 
-from tubo import *
-from vehiculo import *
-from estacionamiento import *
-<<<<<<< HEAD
-mi_carro1 = Vehiculo("carabobo4",5,"toyota",2008,"negro",1234)
-mi_carro2 = Vehiculo("caracas1",3,"toyota",2010,"blanco",8456)
-mi_carro3 = Vehiculo("valencia2",7,"ford",2012,"azul",123567)
+from ordenamiento import *
 
-mi_tubo1 = Tubo(15,"roro")
-(mi_tubo1.Cercano())
-(mi_tubo1.Retirar())
+x=[5,4,3,2,1,0,4,8,-5,6,-9,8,5,2,1,4,5,7,4,1,4]
 
-
-
-estacionamiento1 = Estacionamiento(1)
-(estacionamiento1.Estacionar(mi_carro1))
-(estacionamiento1.Estacionar(mi_carro2))
-(estacionamiento1.Estacionar(mi_carro3))
-(estacionamiento1.Estacionar(mi_carro1))
-(estacionamiento1.Estacionar(mi_carro2))
-(estacionamiento1.Estacionar(mi_carro3))
-estacionamiento1.mostrar()
-
-estacionamiento1.Vaciar(0)
-(estacionamiento1.Estacionar(mi_carro1))
-estacionamiento1.mostrar()
-
-
-=======
-from evento import *
-
-x = Evento()
-print(x.ProcesarLlegadas("prueba.txt"))
->>>>>>> d7e2389999584ae05a47c31bdaea024ac79201d4
+print(x)
+mergesort(x,cmpf)
+print(x)
